05d2c63a799658233c2895ea28b260e3ac566ed6
kirill <6606759@mail.ru>
2024-01-09 14:57:07 +0300

    second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Users from "./components/Users";
+import AddUser from "./components/AddUser";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+        users: [
+            {
+                id: 1,
+                firstname: 'Bob',
+                lastname: 'Marley',
+                bio: 'Lorem ipsum dlor',
+                age: 40,
+                isHappy: true
+            },
+            {
+                id: 2,
+                firstname: 'John',
+                lastname: 'Doe',
+                bio: 'Lorem ipsum dolor',
+                age: 22,
+                isHappy: false
+            }
+        ]
+
+    }
+    this.addUser = this.addUser.bind(this)
+    this.deleteUser = this.deleteUser.bind(this)
+    this.editUser = this.editUser.bind(this)
 }
 
-export default App;
+  render () {
+    return (<div>
+    <Header />
+    <main>
+      <Users users={this.state.users} onEdit={this.editUser} onDelete={this.deleteUser}/>  </main>
+    <aside>
+        <AddUser onAdd={this.addUser}/>
+      </aside>
+</div>)
+  }
+  deleteUser(id) {
+    this.setState({
+      users: this.state.users.filter((el) => el.id !== id)
+    })
+  }
+  editUser(user) {
+    let allUsers = this.state.users
+    allUsers[user.id-1] = user
+
+    this.setState({users: []}, () => {
+      this.setState({users: [...allUsers]})
+    })
+  }
+  addUser(user) {
+    const id = this.state.users.length + 1
+    this.setState({ users: [...this.state.users, {id, ...user}]})
+  }
+}
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class Header extends React.Component {
+  render () {
+    return (
+      <header className='header'>
+        Список пользователей
+        </header>
+    )
+  }
+}
+
+
+export default Header
